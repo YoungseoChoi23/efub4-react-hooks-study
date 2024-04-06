411b625db3b7ecbbb06e666759093121f486c8ab
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-04-06 11:59:17 +0900

    useAxios Hook

---
+++ useAxios/useAxios.js
@@ -0,0 +1,32 @@
+const useAxios = (options, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  if (!options.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+  useEffect(() => {
+    axiosInstance(options)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data: data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error: error });
+      });
+  }, [trigger]);
+  return { ...state, refetch };
+};
